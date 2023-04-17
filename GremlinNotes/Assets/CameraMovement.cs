8e691caf094f90b3835f4a741da443093f415e8a
Patrick Astermann <patrick.astermann01@gmail.com>
2023-04-17 21:49:22 +1000

    ✨Initial Setup and Push
    As Above ⬆

---
+++ GremlinNotes/Assets/CameraMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    void Start()
+    {
+        Camera camera = gameObject.GetComponent<Camera>();  
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
